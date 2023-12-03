e2b10524dfb72c658709d2fa773c700a271b58ea
Gildemberg Leite <gildembergleite@gmail.com>
2023-12-03 11:31:15 -0300

    feat: add link in logo

--- src/components/Header/logo.tsx
+++ src/components/Header/logo.tsx
@@ -1,14 +1,17 @@
 'use client'
 
 import { Separator } from '@radix-ui/react-separator'
+import Link from 'next/link'
 
 export default function Logo() {
   return (
     <>
       <Separator className='bg-input w-full h-0.5' />
-      <h1 className="font-bold text-[12rem] inline-block leading-none">
+      <Link href="/">
+        <h1 className="font-bold text-[12rem] inline-block leading-none">
           THE BLOG
-      </h1>
+        </h1>
+      </Link>
       <Separator className='bg-input w-full h-0.5' />
     </>
   )
