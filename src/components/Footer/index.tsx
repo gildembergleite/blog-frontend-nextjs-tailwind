9c3df2213e25a2fb9a99d93f119558fd25c68096
Gildemberg Leite <gildembergleite@gmail.com>
2023-12-03 16:03:45 -0300

    feat: add links on repo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,15 +4,19 @@ export default function Footer() {
   return (
     <footer className="flex flex-col w-full justify-center items-center">
       <div className="flex flex-col w-full max-w-7xl justify-center items-center px-12 py-6">
-        <p>
-          Make with ❤️ by {' '}
+        <p className='text-center leading-relaxed'>
+            Back-end code on {' '}
+          <Link className='hover:underline' target='_blank' href="https://github.com/gildembergleite/blog-backend-strapi">
+            GitHub
+          </Link>. <br />
+            Front-end code on {' '}
+          <Link className='hover:underline' target='_blank' href="https://github.com/gildembergleite/blog-frontend-nextjs-tailwind">
+            GitHub
+          </Link>. <br />
+            Make with ❤️ by {' '}
           <Link className='hover:underline' target='_blank' href="https://www.gildembergleite.com.br/">
             Gildemberg Leite
-          </Link>.
-          The source code is available on {' '}
-          <Link className='hover:underline' target='_blank' href="#">
-            GitHub
-          </Link>.
+          </Link>. <br />
         </p>
       </div>
     </footer>
