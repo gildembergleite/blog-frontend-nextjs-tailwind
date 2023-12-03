221f85b88c610e3f14bb7f6a9dc3f66729433b3e
Gildemberg Leite <gildembergleite@gmail.com>
2023-12-03 15:11:41 -0300

    feat: grid responsive

--- src/app/posts/[slug]/page.client.tsx
+++ src/app/posts/[slug]/page.client.tsx
@@ -13,7 +13,7 @@ export default function PostsByAuthorPageClient({ populatePosts }: PostsByAuthor
   return (
     <main>
       <MainTitle title={`All posts by: ${populatePosts[0].author.name}`} />
-      <div className='px-12 py-6 w-full justify-center items-center max-w-7xl grid grid-cols-3 gap-6'>
+      <div className='px-12 py-6 w-full justify-center items-center max-w-7xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
         {populatePosts.map((post) => (
           <PostCard key={post.id}
             cover={post.cover_url}
