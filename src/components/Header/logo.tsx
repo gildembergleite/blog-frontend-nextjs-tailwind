5b848f1327350dfea7826f0c740cc73e0d8527f7
Gildemberg Leite <gildembergleite@gmail.com>
2023-12-03 15:06:27 -0300

    style: responsive logo

--- src/components/Header/logo.tsx
+++ src/components/Header/logo.tsx
@@ -8,7 +8,7 @@ export default function Logo() {
     <>
       <Separator className='bg-input w-full h-0.5' />
       <Link href="/">
-        <h1 className="font-bold text-[12rem] inline-block leading-none">
+        <h1 className="font-bold text-[14vw] xl:text-[10vw] inline-block leading-none py-4 md:py-0">
           THE BLOG
         </h1>
       </Link>
