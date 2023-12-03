24b770b29bfbcdcf4542cfaa83fa8fad5aad2882
Gildemberg Leite <gildembergleite@gmail.com>
2023-12-03 07:07:11 -0300

    feat: create footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,20 @@
+import Link from 'next/link'
+
 export default function Footer() {
   return (
-    <div>
-      Enter
-    </div>
+    <footer className="flex flex-col w-full justify-center items-center">
+      <div className="flex flex-col w-full max-w-7xl justify-center items-center px-12 py-6">
+        <p>
+          Make with ❤️ by {' '}
+          <Link className='hover:underline' target='_blank' href="https://www.gildembergleite.com.br/">
+            Gildemberg Leite
+          </Link>.
+          The source code is available on {' '}
+          <Link className='hover:underline' target='_blank' href="#">
+            GitHub
+          </Link>.
+        </p>
+      </div>
+    </footer>
   )
 }
